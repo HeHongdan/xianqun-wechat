0e9549a1751b5d66d0c9f2845aa531ee429b9db5
HHD <hehongdan@live.com>
2018-05-27 20:01:26 +0800

    3-7获取 JSON 数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,20 @@
 Page({
-  onLoad(){
-    console.log("Hello 小程序！")
+  
+  //生命周期onLoad
+  onLoad() {
+    //微信get请求
+    wx.request({
+      //请求接口
+      url: 'https://test-miniprogram.com/api/weather/now',
+      //请求参数
+      data: {
+        city: '广州市'
+      },
+      //请求成功（箭头函数，参数：res）
+      success: res => {
+        console.log(res)
+      }
+    })
   }
+
 })
