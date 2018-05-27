2f7cecd66f5a2b36fa025f19c65b182d61c55a57
HHD <hehongdan@live.com>
2018-05-27 20:17:02 +0800

    3-9展示动态变量

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,10 @@
 Page({
+
+//动态绑定数据(对应.wxml文件)
+  data:{
+    nowTemp : 14,
+    nowWeather : "多云"
+  },
   
   //生命周期onLoad
   onLoad() {
@@ -13,7 +19,7 @@ Page({
       //请求成功（箭头函数，参数：res）
       success: res => {
         console.log(res)
-
+        //取出data标签下的result赋值给result（var：全局变量、let：局部(块级)变量、const ：声明常量(块级)）
         let result = res.data.result
         let temp = result.now.temp
         let weather = result.now.weather
