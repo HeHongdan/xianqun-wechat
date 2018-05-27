2cc224035d0999100405fc4e483fc135894f07c0
HHD <hehongdan@live.com>
2018-05-27 20:28:07 +0800

    3-11更新气温和天气数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,21 @@
+//字符映射关系
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
 Page({
 
-//动态绑定数据(对应.wxml文件)
-  data:{
-    nowTemp : 14,
-    nowWeather : "多云"
+  //动态绑定数据(对应.wxml文件)
+  data: {
+    nowTemp: '14℃',
+    nowWeather: '多云'
   },
-  
+
   //生命周期onLoad
   onLoad() {
     //微信get请求
@@ -18,13 +28,23 @@ Page({
       },
       //请求成功（箭头函数，参数：res）
       success: res => {
+        //打印日志
         console.log(res)
         //取出data标签下的result赋值给result（var：全局变量、let：局部(块级)变量、const ：声明常量(块级)）
         let result = res.data.result
         let temp = result.now.temp
         let weather = result.now.weather
 
-        console.log(temp,weather)
+        console.log(temp, weather)
+
+        //异步改变显示内容
+        this.setData({
+          //字符拼接
+          nowTemp: temp + '℃',
+          //使用映射关系赋值
+          nowWeather: weatherMap[weather]
+        })
+
       }
     })
   }
