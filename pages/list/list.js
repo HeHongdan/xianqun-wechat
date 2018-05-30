1e87a511b8c713a7580d194a0591508cbc3da3aa
HHD <hehongdan@live.com>
2018-05-30 10:45:45 +0800

    5-7使用网络数据

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,66 +1,73 @@
-// pages/list/list.js
+const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六',]
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    weekWeather:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad() {
+    this.getWeekWeather()
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 下拉刷新
    */
-  onReady: function () {
-  
+  onPullDownRefresh(){
+    this.getWeekWeather(() => {
+      wx.stopPullDownRefresh()
+    })
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 请求网络数据
    */
-  onShow: function () {
-  
+  getWeekWeather(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/future',
+      data:{
+        time:new Date().getTime(),
+        city:'广州市',
+      },
+
+      success: res => {
+        let result = res.data.result
+        this.setWeekWeather(result)
+      },
+
+      complete: () => {
+        callback && callback()
+      },
+
+    })
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 设置内容
    */
-  onHide: function () {
-  
-  },
+  setWeekWeather(result) {
+    let weekWeather = []
+    for (let i = 0; i < 7; i++) {
+      let date = new Date()
+      date.setDate(date.getDate()+i)
+      weekWeather.push({
+        day: dayMap[date.getDay()],
+        date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+        temp: `${result[i].minTemp}° - ${result[i].maxTemp}°`,
+        iconPath: '/images/' + result[i].weather + '-icon.png',
+      })
+    }
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    weekWeather[0].day = `今天`
+    this.setData({
+      weekWeather
+    })
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
